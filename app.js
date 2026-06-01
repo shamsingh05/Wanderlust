fd04c41c7ce00ccd4ed92dc300d6c1957a5a946c
samsonremix17 <shambhavi.singh_cs23@gla.ac.in>
2026-06-01 20:27:40 +0530

    Restructured the code into listings and review routes

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
-const Listing = require("./models/listings.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");//destructuring
-const Review = require("./models/review.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -34,132 +33,8 @@ app.get("/",(req,res)=>{
     res.send("root ");
 }) 
 
-const validateListing = (req,res,next) => {
-
-    if(!req.body){
-        return next(
-            new ExpressError(400,"Request body is missing")
-        );
-    }
-    const { error } = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details
-            .map(el => el.message)
-            .join(", ");
-
-        throw new ExpressError(400, errMsg);
-    }
-    next();
-}
-const validateReview = (req,res,next) => {
-
-    if(!req.body){
-        return next(
-            new ExpressError(400,"Request body is missing")
-        );
-    }
-    const { error } = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details
-            .map(el => el.message)
-            .join(", ");
-
-        throw new ExpressError(400, errMsg);
-    }
-    next();
-}
-
-//Index route
-app.get("/listings", wrapAsync( async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index", { allListings });
-}))
-
-//New Route
-app.get("/listings/new", (req,res)=>{
-    res.render("listings/new.ejs")
-});
-
-//Show Route
-app.get("/listings/:id", wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing}); 
-}))
-
-//Create Route
-app.post(
-    "/listings",
-    validateListing, 
-    wrapAsync(async (req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-}))
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-}))
-
-//Update Route
-app.put("/listings/:id", validateListing, wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}))
-
-//delete route
-app.delete("/listings/:id",wrapAsync( async(req,res)=>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}))
-
-//REVIEWS
-//POST REVIEW ROUTE
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async (req, res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    console.log("new ewview saved");
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-//DELETE REVIEW ROUTE
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync( async(req,res)=>{
-    let { id, reviewId} = req.params;
-
-    await Listing.findByIdAndUpdate( id, {$pull: {reviews: reviewId}});//removes review from listings' array
-    await Review.findByIdAndDelete(reviewId);// removes the review from the db
-    res.redirect(`/listings/${id}`);
-} ))
-// app.get("/testListing", async (req,res)=>{
-//     let sampleListing = new Listing({
-//         title: "My new Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India",
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful testing");
-// })
-
-// app.post("/test", (req,res)=>{
-//     console.log(req.headers);
-//     console.log(req.body);
-//     res.send("ok");
-// });
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
